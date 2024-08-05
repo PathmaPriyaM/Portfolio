2eb953d80508773634ada71d238285024caa73c8
M PATHMA PRIYA <66048772+PathmaPriyaM@users.noreply.github.com>
2024-08-05 11:21:55 +0530

    portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import Footer from "./components/Footer";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
