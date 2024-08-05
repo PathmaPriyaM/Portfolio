852c39898c99fd6ced1059d8eb5e9835fc362f04
M PATHMA PRIYA <66048772+PathmaPriyaM@users.noreply.github.com>
2024-08-05 11:19:31 +0530

    portfolio

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,6 +5,7 @@ import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 import Image from "next/image";
+
 const navLinks = [
   {
     title: "About",
